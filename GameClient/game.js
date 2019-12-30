0fa4f0e379e671258c6dfe98aa0357d820f80245
unknown <JoshKPrier@gmail.com>
2019-12-30 16:10:33 -0500

    Added more starter files

--- GameClient/game.js
+++ GameClient/game.js
@@ -1,1 +1,20 @@
 // Contains all logic for parameters (indiviual parameter logic may be contained in seperate module)
+
+//let gameSettings = require('./gameSettings.js');
+
+const Game = function() {
+  this.settings = 0;//gameSettings.getGameState();
+  this.update = function() {
+    console.log("Game Update");
+  };
+};
+Game.prototype = { constructor : Game };
+
+// class Game {
+//   constructor() {
+//     this.settings = 0;
+//   }
+//   update() {
+//     console.log("Game Update");
+//   }
+// }
--- GameClient/main.js
+++ GameClient/main.js
@@ -1,1 +1,50 @@
 // Main will call all needed update calls to game and to display
+
+
+/// OBJECTS
+
+//let controller = new Controller();
+let game = new Game();
+//let display = new Display();
+//let engine = new Engine();
+
+/// INIT
+
+let scale = 1; //Scale of the game canvas (1 will fill the full view)
+
+/// FUNCTIONS
+
+let keyPress = function(event) {
+  //controller.keyPress(event.type, event.keyCode);
+  console.log("keypress")
+};
+
+let resize = function(event) {
+  // display.resize(document.documentElement.clientWidth,
+  //                document.documentElement.clientHeight,
+  //                scale);
+  // display.render();
+  console.log("resize")
+};
+
+let render = function() {
+
+  //TODO display.drawObjects
+
+  // display.render();
+  console.log("render")
+};
+
+let update = function() {
+
+  //TODO: if controller active change game state
+
+  game.update();
+
+  // TODO: add a condition to stop the engine (an end goal parameter)
+};
+
+window.addEventListener("keydown", keyPress);
+window.addEventListener("resize", resize);
+
+update();

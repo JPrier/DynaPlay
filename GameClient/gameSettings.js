3b50e3eba718eeecf857dec465064af36067f093
unknown <JoshKPrier@gmail.com>
2019-12-30 13:06:53 -0500

    Commiting starter files

---
+++ GameClient/gameSettings.js
@@ -0,0 +1,18 @@
+let _gameParams = {
+  'worldType': -1, // -1: Null, 0: Grid, 1: Hex, 2: linear
+  'MovementMethod': -1 //-1:Null, 0: key/button, 1: linear mouse/touch
+}
+
+exports.getGameParams = function() {
+  return _gameParams;
+};
+
+exports.setGameParams = function(params) {
+  //TODO: Validate params
+  _gameParams[worldType] = params[worldType];
+};
+
+// --- USE WITH ---
+// var gameSettings = require('./gameSettings.js');
+//
+// gameSettings.getGameState();

23ba74d460505ba71893b68a40f03ce979b865d6
unknown <JoshKPrier@gmail.com>
2020-01-01 17:35:24 -0500

    Added more rendering logic to display

--- GameClient/display.js
+++ GameClient/display.js
@@ -1,11 +1,35 @@
 // Carries all the logic for drawing and rendering visuals
 
-const Display = function(canvas) {
+const Display = function(canvas, color) {
   this.buffer = document.createElement("canvas").getContext("2d");
   this.context = canvas.getContext("2d");
 
-  // TODO: figure out how to do this for a general method that will allow tiles and non tiles
-}
+  //Set background color
+  this.buffer.fillStyle = color;
+  this.buffer.fillRect(0, 0, this.buffer.canvas.width, this.buffer.canvas.height);
+
+  this.drawMap = function(map) {
+    // TODO: create a generic map object that can be used to create random maps
+    //for (let i = map.length - 1; i > -1; -- i) {
+      //this.drawObject(map[i].shape, ...)
+    //}
+  }
+
+  this.drawObject = function(shape, source_x, source_y, destination_x,
+                             destination_y, width, height) {
+      //TODO: add switch cases for each shape and draw each shape onto the buffer
+  }
+
+  this.resize = function(width, height, height_width_ratio) {
+    if (height/width > height_width_ratio) {
+      this.context.canvas.height = width * height_width_ratio;
+      this.context.canvas.width = width;
+    } else {
+      this.context.canvas.height = height;
+      this.context.canvas.width = height / height_width_ratio;
+    }
+  };
+};
 
 Display.prototype = {
   constructor: Display,
--- GameClient/main.js
+++ GameClient/main.js
@@ -8,19 +8,20 @@ let keyPress = function(event) {
 };
 
 let resize = function(event) {
-  // display.resize(document.documentElement.clientWidth,
-  //                document.documentElement.clientHeight,
-  //                scale);
-  // display.render();
-  console.log("resize")
+  display.resize(document.documentElement.clientWidth,
+                 document.documentElement.clientHeight,
+                 scale);
+  display.drawObject();
+  render();
 };
 
 let render = function() {
 
-  //TODO display.drawObjects
+  //TODO draw game map (static objects in map array)
 
-  // display.render();
-  //console.log("render")
+  //TODO draw objects (players, npcs, etc)
+
+  display.render();
 };
 
 let update = function() {
@@ -28,6 +29,7 @@ let update = function() {
   //TODO: if controller active change game state
 
   game.update();
+  resize();
 
   // TODO: add a condition to stop the engine (an end goal parameter)
 };
@@ -36,7 +38,7 @@ let update = function() {
 
 //let controller = new Controller();
 let game = new Game();
-//let display = new Display();
+let display = new Display(document.querySelector("canvas"), "black");
 let engine = new Engine(1000/30, update, render);
 
 /// INIT

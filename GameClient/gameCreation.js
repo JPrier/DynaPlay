26d90bebd7f1401a9fabd7a7d2b287604aca447d
Josh <JoshKPrier@gmail.com>
2020-02-17 12:09:09 -0500

    Cleaned up comments

--- GameClient/gameCreation.js
+++ GameClient/gameCreation.js
@@ -1,26 +1,25 @@
 // Holds all methods used by the frontend for validating and setting game settings
 
 let settings = {
-  "worldType": [[0,1,2,3], ["Grid","Hex","Linear","Platformer"]],
-  "movementMethod": [[-1,0,1], ["None","Button/Keyboard","Mouse or Drag(TouchScreen)"]],
-  "playerVS": [[-1,0,1], ["None","PvP","PvE"]],
-  "NPCs": [0,99],
-  "NPCBehavior": [[-1,0,1,2,3], ["Do Nothing","Attack Player","Follow Player","Attack other NPCs","Collect Items"]],
-  "NPCDifficulty": [[0,1,2], ["Easy","Medium","Hard"]],
-  "scoring": [[0,1,2,3], ["Collect Items","Kill other players","Kill NPCs","Surive (time)"]],
-  "movement": [[0,1], ["Linear","Turn Based"]],
-  "playerColor": "white",
-  "backgroundColor": "black",
-  "NPCColor": "blue",
-  "itemColor": "green",
-  "endGoal": [[0,1,2], ["Hit point amount","Die","Last Alive"]],
+  // "worldType": [[0,1,2,3], ["Grid","Hex","Linear","Platformer"]],
+  // "movementMethod": [[-1,0,1], ["None","Button/Keyboard","Mouse or Drag(TouchScreen)"]],
+  // "playerVS": [[-1,0,1], ["None","PvP","PvE"]],
+  // "NPCs": [0,99],
+  // "NPCBehavior": [[-1,0,1,2,3], ["Do Nothing","Attack Player","Follow Player","Attack other NPCs","Collect Items"]],
+  // "NPCDifficulty": [[0,1,2], ["Easy","Medium","Hard"]],
+  // "scoring": [[0,1,2,3], ["Collect Items","Kill other players","Kill NPCs","Surive (time)"]],
+  // "movement": [[0,1], ["Linear","Turn Based"]],
+  // "playerColor": "white",
+  // "backgroundColor": "black",
+  // "NPCColor": "blue",
+  // "itemColor": "green",
+  // "endGoal": [[0,1,2], ["Hit point amount","Die","Last Alive"]],
   "fillPercent": [0,100],
   "randomMethod": [[0,1,2,3,4,5], ["Perlin Noise", "Simplex Noise", "Random", "Cubic Noise", "Buffered Cubic Noise", "Octave Perlin"]]
 }
 
 //Set Body programatically using the gameParams
 
-//TODO: condense and clean up the string creation
 let bodyInner = "";
 let keys = Object.keys(settings);
 for (let i=0;i<keys.length;i++) {
@@ -62,8 +61,6 @@ let validateParams = function() {
 }
 
 let startGame = function(settings) {
-  document.body.innerHTML = '<canvas></canvas>';//'<canvas width="300" height="300"></canvas>';
+  document.body.innerHTML = '<canvas></canvas>';
   mainSetup(settings);
 }
-
-//startGame(gameSettings);
